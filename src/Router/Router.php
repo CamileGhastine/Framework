e652bec5319042de1e08f2749096e6ad279a2642
CamileGhastine <camileghastine@hotmail.com>
2021-11-04 12:41:46 +0100

    refactor router

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -4,24 +4,25 @@
 namespace Souris\Router;
 
 
-use App\Controller\HomeController;
-use App\Controller\ErrorController;
+
 
 class Router
 {
     public function run()
     {
+
         $url = $_SERVER['REQUEST_URI'];
 
         if ($url === '/') {
-            $homeController = new HomeController();
-            $homeController->home();
-        } elseif ($url === '/pagetest') {
-            $homeController = new HomeController();
-            $homeController->pageTest();
+            $controller = 'App\Controller\HomeController';
+            $action = 'home';
         } else {
-            $errorController = new ErrorController();
-            $errorController->page404();
+            $UriTerms = explode('/', $url);
+            $controller = 'App\Controller\\' . ucfirst($UriTerms[1]) . 'Controller';
+            $action = $UriTerms[2];
         }
+ 
+        $controller = new $controller();
+        $controller->$action();
     }
 }
