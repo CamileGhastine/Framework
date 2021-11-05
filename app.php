ecc97057a031c52aed1c3c7a7d6b739c4d622976
EricJaslet <eric.jaslet@lean.fr>
2021-11-05 16:38:05 +0100

    delete error

--- app.php
+++ app.php
@@ -19,8 +19,6 @@ use Symfony\Component\Dotenv\Dotenv;
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/.env');
 
-var_dump($_ENV['HECTO']);
-
 $container['request'] = function () {
     return new Request();
 };
