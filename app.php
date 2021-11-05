d50a0226db8e03bd013e4000812327fd822a0f38
EricJaslet <eric.jaslet@lean.fr>
2021-11-05 16:37:01 +0100

    up

--- app.php
+++ app.php
@@ -14,6 +14,12 @@ use Twig\Loader\FilesystemLoader;
 
 $container = new Container;
 
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+var_dump($_ENV['HECTO']);
 
 $container['request'] = function () {
     return new Request();
