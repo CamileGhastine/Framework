2659c7579c96fc4cedcd87cb83808ca8a874ce83
CamileGhastine <camileghastine@hotmail.com>
2021-11-10 14:04:16 +0100

    refactor AbstractController

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -6,23 +6,27 @@ use Souris\Container;
 
 abstract class AbstractController
 {
+    protected Twig $twig;
+
     public function __construct(protected Container $container)
     {
     }
 
     public function render(string $view, array $params = [])
     {
-        //A refacto
         $twig = $this->container['twig'];
-        $class = explode('\\', get_class($this));
-        $class = end($class);
-        $method = debug_backtrace()[1]['function'];
-        $baseTwig= $class.'.'.$method;
-        $baseTwig = isset($this->container['config'][$baseTwig]) ? $class.'.'.$method : 'default';
-        //
-        
+        $baseTwig = $this->findBaseTwig($twig, get_class($this), debug_backtrace()[1]['function']);
+
         $template = $twig->load($this->container['config'][$baseTwig]);
         $template = $twig->load($view);
         return $template->render($params);
+    }
+
+    private function findBaseTwig($twig, $class, $method)
+    {
+        $class = explode('\\', $class);
+        $class = end($class);
+        $baseTwig = $class . '.' . $method;
+        return  isset($this->container['config'][$baseTwig]) ? $class . '.' . $method : 'default';
     }
 }
